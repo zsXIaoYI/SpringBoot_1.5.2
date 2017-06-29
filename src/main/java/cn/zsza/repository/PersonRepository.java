bc80526aaf842f2eb2bdb36c9d85fd07a9f01e0a
zhangsong <shanchong@maizijf.com>
2017-06-29 18:29:06 +0800

    添加一些方法

--- src/main/java/cn/zsza/repository/PersonRepository.java
+++ src/main/java/cn/zsza/repository/PersonRepository.java
@@ -5,6 +5,7 @@ import cn.zsza.entity.Person;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
@@ -20,5 +21,16 @@ public interface PersonRepository extends JpaRepository<Person,Long> {
     public Person findByName(String name);
 
     Page<Person> findAll(Pageable pageable);
+
+
+    Person findByNameAndAddressAndAge(String name, String address, int age);
+
+    @Query("select p.name from Person p where p.age in ?1")
+    List<String> findNameByAges(List<Integer> ages);
+
+
+    @Query("select count(id) from Person  where name = ?1 and age = ?2")
+    Integer getCountByNameAndAge(String name,Integer age);
+
 
 }
