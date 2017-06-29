6e1b53c9c5525c7ead3b9ce814d55e45ce23eb7f
zhangsong <shanchong@maizijf.com>
2017-06-29 18:26:20 +0800

    添加lombok支持

--- src/main/java/cn/zsza/entity/Person.java
+++ src/main/java/cn/zsza/entity/Person.java
@@ -1,15 +1,15 @@
 package cn.zsza.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-
+import lombok.Data;
 import javax.persistence.*;
 import java.util.Date;
-
 /**
  * Created by zs on 2017/2/28.
  * 16:56
  */
 @Entity
+@Data
 @Table(name = "person")
 public class Person {
     public Person(){
@@ -19,35 +19,18 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "age")
+    private int age;
+
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "create_date",insertable = false)
     private Date createDate;
 
 
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
 }
