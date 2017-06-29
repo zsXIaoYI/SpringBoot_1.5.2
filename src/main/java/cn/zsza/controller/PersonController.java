cb1071b1b4e8583cb96dcf89e7d1fd73e7b61634
zhangsong <shanchong@maizijf.com>
2017-06-29 18:31:44 +0800

    去掉空格

--- src/main/java/cn/zsza/controller/PersonController.java
+++ src/main/java/cn/zsza/controller/PersonController.java
@@ -12,11 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Arrays;
 import java.util.List;
-
-
 /**
  * Created by zs on 2017/2/28.
  * 16:59
