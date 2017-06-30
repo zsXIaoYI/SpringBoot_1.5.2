8c86f5f53e2e058417a3aab4742cde20fb9088ea
zhangsong <shanchong@maizijf.com>
2017-06-30 11:22:24 +0800

    修改

--- src/main/java/cn/zsza/service/PersonService.java
+++ src/main/java/cn/zsza/service/PersonService.java
@@ -1,18 +1,17 @@
 package cn.zsza.service;
 
-
 import cn.zsza.entity.Person;
 import cn.zsza.repository.PersonRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-
 /**
  * Created by zs on 2017/3/6.
  * 15:17
  */
 @Service
+@Log4j2
 public class PersonService {
 
     @Autowired
@@ -24,7 +23,9 @@ public class PersonService {
 
     public Person getPersonByName(String name){
 
-        System.out.println(1111);
+        log.info("name is {}",name);
+        log.warn("warn message");
+
         return personRepository.findByName(name);
     }
 }
